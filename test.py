805737f83a2f683fd94d5a0388724ab13bab70df
Andrewmakmaer <andrewmakmaer@gmail.com>
2021-06-28 01:39:19 +0300

    latest stable version

--- test.py
+++ test.py
@@ -1,19 +1,40 @@
 import unittest
-from main import main
+from main import main, make_few_words
 
 
 class TestAddCase(unittest.TestCase):
     def test_1(self):
-        res = main("продАЖ(ный)", "с")
-        self.assertEqual(res, "продАЖ{'ный'}\n")
+        res = main("продАЖ()", "с")
+        self.assertEqual(res, "продАЖ{''}\n")
 
     def test_2(self):
         res = main("арбитрАЖный()", "с")
-        self.assertEqual(res, "продАЖ{'ный'}\nарбитрАЖный{''}\n")
+        self.assertEqual(res, "продАЖ{''}\nарбитрАЖный{''}\n")
 
     def test_3(self):
-        res = main("продАЖ(а)", "с")
-        self.assertEqual(res, "арбитрАЖный{''}\nпродАЖ{'а', 'ный'}\n")
+        res = main("продАЖ()", "с")
+        self.assertEqual(res, "арбитрАЖный{''}\nпродАЖ{''}\n")
 
     def test_4(self):
-        res = main()
+        res = main("пАЧе()", "с")
+        self.assertEqual(res, "пАЧе{''}\n")
+
+    def test_5(self):
+        res = main("пАтЧ()", "с")
+        self.assertEqual(res, "пАЧе{''}\nпАтЧ{''}\n")
+
+    def test_6(self):
+        res = main("ЧАд()", "с")
+        self.assertEqual(res, "ЧАд{''}\n")
+
+    def test_7(self):
+        res = main("тАнЦы()", "с")
+        self.assertEqual(res, "тАнЦы{''}\n")
+
+    def test_8(self):
+        res = main("абзАЦ()", "с")
+        self.assertEqual(res, "тАнЦы{''}\nабзАЦ{''}\n")
+
+    # def test_9(self):
+    #     t1, t2 = make_few_words("")
+
